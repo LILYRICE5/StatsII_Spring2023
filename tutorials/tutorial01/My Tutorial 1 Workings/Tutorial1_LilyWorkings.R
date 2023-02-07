69f60ef0843232a33df653bea3d5d0b6569f414d
LILYRICE5 <lrice@tcd.ie>
2023-02-07 12:02:45 +0000

    updates

--- tutorials/tutorial01/My Tutorial 1 Workings/Tutorial1_LilyWorkings.R
+++ tutorials/tutorial01/My Tutorial 1 Workings/Tutorial1_LilyWorkings.R
@@ -1,16 +1,28 @@
+## Tutorial 1 - Revision - Lily ##
+
+# FIRST - LOAD LIBRARIES
+
 library(tidyverse)
+
+install.packages("stargazer")
 library(stargazer)
+
+?stargazer
+# 1. IMPORT DATA:
 
 data <- read_csv("data/tutorial1_data.csv", 
                  col_types = cols(
                    `Ease of doing business rank (1=most business-friendly regulations) [IC.BUS.EASE.XQ]` = col_double(),
                    `Tax revenue (% of GDP) [GC.TAX.TOTL.GD.ZS]` = col_double(),
                    `GDP per capita (current US$) [NY.GDP.PCAP.CD]` = col_double()))
+
 data <- data %>%
   select(-(starts_with("Time")), -(`Country Code`))
 
 names(data) <- sub(" \\[.*", "", names(data))
 
+# DATA ANALYSIS: 
+## 1. GGPLOT TO CREATE SCATTERPLOT OF GDP VS TAX REVENUE, ADDING SIMPLE LM LINE
 data %>%
   ggplot(aes(`Tax revenue (% of GDP)`, `GDP per capita (current US$)`)) +
   geom_point() +
@@ -28,12 +40,15 @@ data %>%
   geom_point() +
   geom_smooth(method = "lm")
 
+#creates a value titled "formula" (see in Global Environment)
 formula <- `GDP per capita (current US$)` ~ `Tax revenue (% of GDP)` + `Ease of doing business rank (1=most business-friendly regulations)`
 
+#run the regression (linear regression, additive model)
 reg <- lm(formula, data)
 
 stargazer(reg, type = "latex")
 
+#just labeling the graph
 data %>%
   ggplot(aes(`Tax revenue (% of GDP)`, 
              `GDP per capita (current US$)`, 
