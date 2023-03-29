b7ff55bfd56dfc1909aa31a83318de1e293e9a46
LILYRICE5 <lrice@tcd.ie>
2023-03-29 19:10:33 +0100

    replication workings

--- replication/my_replication/JudgingProsecutors_rep.R
+++ replication/my_replication/JudgingProsecutors_rep.R
@@ -1,24 +1,3 @@
-#########################################
-# Final Replication Project - Lily Rice #
-#########################################
-
-################################################
-
-################################################
-
-## Install Packages
-install.packages("haven")
-install.packages("tidyverse")
-install.packages("ggplot2")
-install.packages("psych")
-install.packages("survey")
-install.packages("labelled")
-install.packages("knitr")
-install.packages("stargazer")
-install.packages("sjPlot")
-install.packages("gridExtra")
-
-## Load necessary libraries
 library(haven)
 library(tidyverse)
 library(ggplot2)
@@ -42,7 +21,7 @@ ggmjntheme <- theme(axis.title.x=element_text(colour="black",size=14),
 
 cces <- read_dta("JudgingProsecutors.dta")
 
-# Recode and create variables for treatments, outcomes, and demographics
+## Recode and create variables for treatments, outcomes, and demographics
 
 cces <- cces %>% mutate(lenient_pre = as.factor(ifelse(PSU357_treat %in% c(1,3), 1, ifelse(is.na(PSU357_treat), NA, 0))),
                         lenient_post = as.factor(ifelse(PSU431_split %in% c(1,3), 1, ifelse(is.na(PSU431_split), NA, 0))),
@@ -64,7 +43,7 @@ cces <- cces %>% mutate(lenient_pre = as.factor(ifelse(PSU357_treat %in% c(1,3),
                         white = ifelse(race == 1, 1, 0), 
                         black = ifelse(race == 2, 1, 0))
 
-# Create PCA incumbent support measure for both waves
+## Create PCA incumbent support measure for both waves
 
 pca_pre <- pca(cces %>% select(c(approve_pre, fair_pre, elec_sup_pre)), scores = T)
 
@@ -74,37 +53,33 @@ pca_post <- pca(cces %>% select(c(approve_post, fair_post, elec_sup_post)), scor
 
 cces <- cces %>% cbind(pca_post$scores) %>% rename(incum_post = PC1)
 
-# Variability explained by PCA
+### Variability explained by PCA
 
 pca_pre$Vaccounted
 pca_post$Vaccounted
 
-# Construct survey object
+## Construct survey object
 
 cces_design <- svydesign(id = ~1, weights = ~teamweight, data = cces)
 
 # T-tests, treatment effects, and plots
 
-# Study 1
+## Study 1
 svyttest(elec_sup_pre~lenient_pre, cces_design)$p.value
 
-# Effect size
+### Effect size
 svyttest(elec_sup_pre~lenient_pre, cces_design)$estimate 
 
-# Effect size (in terms of standard deviations)
+### Effect size (in terms of standard deviations)
 svyttest(elec_sup_pre~lenient_pre, cces_design)$estimate / sqrt(svyvar(~elec_sup_pre, cces_design, na.rm = T))
 
-# Alternative outcomes (appendix)
+### Alternative outcomes (appendix)
 svyttest(approve_pre~lenient_pre, cces_design)$p.value
 svyttest(fair_pre~lenient_pre, cces_design)$p.value
 svyttest(incum_pre~lenient_pre, cces_design)$p.value
 
-# Figure 1
-ggplot(svyby(~elec_sup_pre,~lenient_pre, cces_design, svymean, na.rm = T)) + 
-  geom_pointrange(aes(x = rownames(svyby(~elec_sup_pre,~lenient_pre, cces_design, svymean, na.rm = T)), 
-                      y=elec_sup_pre, ymin=elec_sup_pre-1.96*se, ymax = elec_sup_pre+1.96*se))+ 
-  xlab("Sentence Length") + ylab("Re-Election Support") + scale_x_discrete(labels = c("6 Months", "6 Days")) + 
-  scale_y_continuous(limits = c(1,4)) + theme_bw() + labs(title = "Re-Election Support") + ggmjntheme
+### Figure 1
+ggplot(svyby(~elec_sup_pre,~lenient_pre, cces_design, svymean, na.rm = T)) + geom_pointrange(aes(x = rownames(svyby(~elec_sup_pre,~lenient_pre, cces_design, svymean, na.rm = T)), y=elec_sup_pre, ymin=elec_sup_pre-1.96*se, ymax = elec_sup_pre+1.96*se))+ xlab("Sentence Length") + ylab("Re-Election Support") + scale_x_discrete(labels = c("6 Months", "6 Days")) + scale_y_continuous(limits = c(1,4)) + theme_bw() + labs(title = "Re-Election Support") + ggmjntheme
 
 ### Figure C1 (all three plots)
 ggplot(svyby(~incum_pre,~lenient_pre, cces_design, svymean, na.rm = T)) + geom_pointrange(aes(x = rownames(svyby(~incum_pre,~lenient_pre, cces_design, svymean, na.rm = T)), y=incum_pre, ymin=incum_pre-1.96*se, ymax = incum_pre+1.96*se))+ xlab("Sentence Length") + ylab("Incumbent Supportl") + scale_x_discrete(labels = c("6 Months", "6 Days")) + scale_y_continuous(limits = c(-3,3)) + theme_bw() + labs(title = "Incumbent Support") + ggmjntheme + geom_hline(yintercept = 0, linetype = "dashed", colour = "grey60")
@@ -113,22 +88,22 @@ ggplot(svyby(~approve_pre,~lenient_pre, cces_design, svymean, na.rm = T)) + geom
 
 ggplot(svyby(~fair_pre,~lenient_pre, cces_design, svymean, na.rm = T)) + geom_pointrange(aes(x = rownames(svyby(~fair_pre,~lenient_pre, cces_design, svymean, na.rm = T)), y=fair_pre, ymin=fair_pre-1.96*se, ymax = fair_pre+1.96*se))+ xlab("Sentence Length") + ylab("Perceived Fairness") + scale_x_discrete(labels = c("6 Months", "6 Days")) + scale_y_continuous(limits = c(1,4)) + theme_bw() + labs(title = "Perceived Fairness") + ggmjntheme
 
-# Study 2
+## Study 2
 svyttest(elec_sup_post~lenient_post, cces_design)$p.value
 svyttest(elec_sup_post~copartisan, cces_design)$p.value
 
-# Effect sizes
+### Effect sizes
 svyttest(elec_sup_post~lenient_post, cces_design)$estimate 
 svyttest(elec_sup_post~copartisan, cces_design)$estimate 
 
-# Effect sizes (in terms of standard deviations)
+### Effect sizes (in terms of standard deviations)
 svyttest(elec_sup_post~lenient_post, cces_design)$estimate / sqrt(svyvar(~elec_sup_post, cces_design, na.rm = T))
 svyttest(elec_sup_post~copartisan, cces_design)$estimate / sqrt(svyvar(~elec_sup_post, cces_design, na.rm = T))
 
-# Figure 2
+### Figure 2
 ggplot(svyby(~elec_sup_post,~copartisan*lenient_post, cces_design, svymean, na.rm = T)) + geom_pointrange(aes(x = rownames(svyby(~elec_sup_post,~copartisan*lenient_post, cces_design, svymean, na.rm = T)), y=elec_sup_post, ymin=elec_sup_post-1.96*se, ymax = elec_sup_post+1.96*se)) + xlab("Treatment Combination") + ylab("Re-Election Support") + scale_x_discrete(labels = c("1 Year \n + Outpartisan", "1 Day \n + Outpartisan", "1 Year \n + Copartisan", "1 Day \n + Copartisan")) + scale_y_continuous(limits = c(1,5)) + theme_bw() + labs(title = "Re-Election Support") + ggmjntheme
 
-# Figure C2 (all three plots)
+### Figure C2 (all three plots)
 ggplot(svyby(~approve_post,~copartisan*lenient_post, cces_design, svymean, na.rm = T)) + geom_pointrange(aes(x = rownames(svyby(~approve_post,~copartisan*lenient_post, cces_design, svymean, na.rm = T)), y=approve_post, ymin=approve_post-1.96*se, ymax = approve_post+1.96*se)) + xlab("Treatment Combination") + ylab("Prosecutorial Approval") + scale_x_discrete(labels = c("1 Year \n + Outpartisan", "1 Day \n + Outpartisan", "1 Year \n + Copartisan", "1 Day \n + Copartisan")) + scale_y_continuous(limits = c(1,5)) + theme_bw() + labs(title = "Prosecutor Approval") + ggmjntheme
 
 ggplot(svyby(~fair_post,~copartisan*lenient_post, cces_design, svymean, na.rm = T)) + geom_pointrange(aes(x = rownames(svyby(~fair_post,~copartisan*lenient_post, cces_design, svymean, na.rm = T)), y=fair_post, ymin=fair_post-1.96*se, ymax = fair_post+1.96*se)) + xlab("Treatment Combination") + ylab("Perceived Fairness") + scale_x_discrete(labels = c("1 Year \n + Outpartisan", "1 Day \n + Outpartisan", "1 Year \n + Copartisan", "1 Day \n + Copartisan")) + scale_y_continuous(limits = c(1,5)) + theme_bw() + labs(title = "Perceived Fairness") + ggmjntheme
@@ -136,13 +111,13 @@ ggplot(svyby(~fair_post,~copartisan*lenient_post, cces_design, svymean, na.rm = 
 ggplot(svyby(~incum_post,~copartisan*lenient_post, cces_design, svymean, na.rm = T)) + geom_pointrange(aes(x = rownames(svyby(~incum_post,~copartisan*lenient_post, cces_design, svymean, na.rm = T)), y=incum_post, ymin=incum_post-1.96*se, ymax = incum_post+1.96*se)) + xlab("Treatment Combination") + ylab("Incumbent Support") + scale_x_discrete(labels = c("1 Year \n + Outpartisan", "1 Day \n + Outpartisan", "1 Year \n + Copartisan", "1 Day \n + Copartisan")) + scale_y_continuous(limits = c(-3,3)) + geom_hline(yintercept = 0, linetype = "dashed", colour = "grey60")  + theme_bw() + labs(title = "Incumbent Support") + ggmjntheme
 
 # Regression models
-# Study 1
+
+## Study 1
 pre_m1c <- glm(elec_sup_pre ~ lenient_pre + white + black + birthyr + pknow + as.factor(gender) + ideo + educ, family = "gaussian", cces)
 pre_m2c <- glm(elec_sup_pre ~ def_white + white + black + birthyr + pknow + as.factor(gender) + ideo + educ, family = "gaussian", cces)
 pre_m3c <- glm(elec_sup_pre ~ lenient_pre*def_white + white + black + birthyr + pknow + as.factor(gender) + ideo + educ, family = "gaussian", cces)
 
-# Table C1
-## Note, this outputs code into the console, I paste this into a .tex file to produce a PDF with this (and all) tables replicated
+### Table C1
 stargazer(pre_m1c, pre_m2c, pre_m3c, dep.var.labels = c("Re-election Support"), title = "Regression Models including Defendant Race, Study 1", header = FALSE, digits=2, style = "apsr", star.cutoffs = c(.05, .01, .001), covariate.labels = c("Lenient Sentence (1 Day)", "White", "Black", "Age", "Pros. Knowledge", "Woman", "Conservatism", "Education"))
 
 pre_elecc <- glm(elec_sup_pre ~ lenient_pre + white + black + birthyr + pknow + as.factor(gender) + ideo + educ, family = "gaussian", cces)
@@ -150,33 +125,32 @@ pre_supc <- glm(incum_pre ~ lenient_pre + white + black + birthyr + pknow + as.f
 pre_appc <- glm(approve_pre ~ lenient_pre + white + black + birthyr + pknow + as.factor(gender) + ideo + educ, family = "gaussian", cces)
 pre_fairc <- glm(fair_pre ~ lenient_pre + white + black + birthyr + pknow + as.factor(gender) + ideo + educ, family = "gaussian", cces)
 
-# Table C2
+### Table C2
 stargazer(pre_elecc, pre_supc, pre_appc, pre_fairc, dep.var.labels = c("Re-election Support", "Incumbent Support", "Approval", "Fairness"), title = "Alternative Outcome Models, Study 1", header = FALSE, digits=2, style = "apsr", star.cutoffs = c(.05, .01, .001), covariate.labels = c("Lenient Sentence (1 Day)", "White", "Black", "Age", "Pros. Knowledge", "Woman", "Conservatism", "Education"))
 
-# Study 2 
+## Study 2 
 post_m1s <- glm(elec_sup_post ~ lenient_post, family = "gaussian", cces %>% filter(copartisan %in% c(0,1)))
 post_m2s <- glm(elec_sup_post ~ copartisan, family = "gaussian", cces %>% filter(copartisan %in% c(0,1)))
 post_m3s <- glm(elec_sup_post ~ lenient_post*copartisan, family = "gaussian", cces %>% filter(copartisan %in% c(0,1)))
 
-# Table 1
+### Table 1
 stargazer(post_m1s, post_m2s, post_m3s, dep.var.labels = c("Re-election Support"), title = "Regression Models, Study 2", header = FALSE, digits=2, style = "apsr", star.cutoffs = c(.05, .01, .001), covariate.labels = c("Lenient Sentence (1 Day)", "Copartisan Pros.", "Copartisan $\\times$ Lenient Sentence"))
 
 post_m1c <- glm(elec_sup_post ~ lenient_post + white + black + birthyr + pknow + as.factor(gender) + ideo + educ, family = "gaussian", cces %>% filter(copartisan %in% c(0,1)))
 post_m2c <- glm(elec_sup_post ~ copartisan + white + black + birthyr + pknow + as.factor(gender) + ideo + educ, family = "gaussian", cces %>% filter(copartisan %in% c(0,1)))
 post_m3c <- glm(elec_sup_post ~ lenient_post*copartisan + white + black + birthyr + pknow + as.factor(gender) + ideo + educ, family = "gaussian", cces %>% filter(copartisan %in% c(0,1)))
 
-# Table C3
+### Table C3
 stargazer(post_m1c, post_m2c, post_m3c, dep.var.labels = c("Re-election Support"), title = "Regression Models with Controls, Study 2", header = FALSE, digits=2, style = "apsr", star.cutoffs = c(.05, .01, .001), covariate.labels = c("Lenient Sentence (1 Day)", "Copartisan Pros.", "White", "Black", "Age", "Pros. Knowledge", "Woman", "Conservatism", "Education", "Copartisan $\\times$ Lenient Sentence"))
-
 
 # Partisanship Effects (Supplementary Material)
 
-## Study 1 (T TESTS)
+## Study 1
 svyttest(elec_sup_pre~lenient_pre, subset(cces_design, dem == 1))
 svyttest(elec_sup_pre~lenient_pre, subset(cces_design, rep == 1))
 svyttest(elec_sup_pre~lenient_pre, subset(cces_design, dem == 0 & rep == 0))
 
-## Study 2 (T TESTS)
+## Study 2
 svyttest(elec_sup_post~lenient_post, subset(cces_design, dem == 1))
 svyttest(elec_sup_post~lenient_post, subset(cces_design, rep == 1))
 
@@ -195,9 +169,9 @@ svyttest(elec_sup_post~lenient_post, subset(cces_design, rep == 1 & copartisan =
 
 ### Study 1 (left-side)
 rbind(cbind(svyby(~elec_sup_pre, ~lenient_pre, subset(cces_design, rep == 1), svymean, na.rm = T), 
-            party = "rep"), 
-      cbind(svyby(~elec_sup_pre, ~lenient_pre, subset(cces_design, dem == 1), svymean, na.rm = T), 
-            party = "dem")) %>% mutate(treatment = lenient_pre) %>%
+                          party = "rep"), 
+                    cbind(svyby(~elec_sup_pre, ~lenient_pre, subset(cces_design, dem == 1), svymean, na.rm = T), 
+                          party = "dem")) %>% mutate(treatment = lenient_pre) %>%
   ggplot(aes(x = treatment, group = treatment, color = as.factor(party))) + 
   geom_pointrange(aes(y = elec_sup_pre, ymin = elec_sup_pre - 1.96*se, ymax = elec_sup_pre + 1.96*se),
                   position = position_dodge2(.5)) +
@@ -218,6 +192,3 @@ rbind(cbind(svyby(~elec_sup_post, ~copartisan*lenient_post, subset(cces_design, 
   scale_x_discrete(labels = c("1 Year \n + Outpartisan", "1 Day \n + Outpartisan", "1 Year \n + Copartisan", "1 Day \n + Copartisan")) +
   theme_bw() + ggmjntheme + xlab("Treatment Combination") + ylim(1,5) + ggtitle("Re-Election Support, Study 2") +
   ylab("Re-Election Support")
-
-
-##### lily workings
