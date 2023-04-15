65ab5f5e500fdcb890e164a13037bc99f97bbc97
LILYRICE5 <lrice@tcd.ie>
2023-04-15 10:34:35 +0100

    created my_answers file

---
+++ problemSets/PS04/my_answers/PS4_my_answers_lilyrice.R
@@ -0,0 +1,12 @@
+##############################
+# Problem Set 4 - My Answers #
+##############################
+
+## Question 1:
+
+# Load dataset:"infants" dataset in the eha library
+
+# Fit a Cox Proportional Hazard Model using mother's age and infant's
+# gender as covariates:
+
+# Present and interpret the output:
